d317a0e186aa4fc5ec767a7b88be98929cedd152
Stive Tenorio <stivetenorio3520@yahoo.com>
2018-12-05 17:27:18 -0600

    Comment on javascript and adjust functions

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-// Remove the 'nojs' class and add the 'js' class
+// remove the 'nojs' class and add the 'js' class
 var html = document.querySelector('html');
 html.classList.remove('nojs');
 html.classList.add('js');
@@ -6,22 +6,21 @@ html.classList.add('js');
 var navigation = document.querySelector('#navigation');
 var nav_list = document.querySelector('.nav');
 
-// Build the outer heading element
+// build the outer heading element
 var nav_heading = document.createElement('h2');
-// Build the inner anchor/link element
+// build the inner link element
 var menu_link = document.createElement('a');
 menu_link.textContent = 'Menu';
 menu_link.setAttribute('id','menu-button');
 menu_link.setAttribute('href','#null');
 menu_link.setAttribute('tabindex', '1');
-// Append the menu_link to the heading element
+// append the menu_link to the heading element
 nav_heading.append(menu_link);
 
-// Finally, append the nav_heading to #navigation
+// append the nav_heading to #navigation
 navigation.insertBefore(nav_heading,nav_list);
 
-// Listen for clicks on #menu-button and toggle the
-// visible class
+// Listen for clicks on #menu-button and toggle the visible class
 var menu_button = document.querySelector('#menu-button');
 menu_button.addEventListener('click', function(e) {
   nav_list.classList.toggle('visible');
@@ -29,14 +28,12 @@ menu_button.addEventListener('click', function(e) {
 })
 
 function myFunction(imgs) {
-  // Get the expanded image
+  // get the expanded image
   var expandImg = document.getElementById("expandedImg");
-  // Get the image text
+  // get the image text
   var imgText = document.getElementById("imgtext");
-  // Use the same src in the expanded image as the image being clicked on from the grid
+  // use the same img src in the expanded image as the image being clicked
   expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
+  // Show the container element
   expandImg.parentElement.style.display = "block";
 }
