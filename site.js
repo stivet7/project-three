284883893570402cc634cb8c75c95479eb566116
Stive Tenorio <stivetenorio3520@yahoo.com>
2018-12-06 18:55:53 -0600

    Further comments

--- site.js
+++ site.js
@@ -6,7 +6,6 @@ html.classList.add('js');
 
 var navigation = document.querySelector('#navigation');
 var nav_list = document.querySelector('.nav');
-
 // build the outer heading element
 var nav_heading = document.createElement('h2');
 // build the inner link element
@@ -17,17 +16,15 @@ menu_link.setAttribute('href', '#null');
 menu_link.setAttribute('tabindex', '1');
 // append the menu_link to the heading element
 nav_heading.append(menu_link);
-
 // append the nav_heading to #navigation
 navigation.insertBefore(nav_heading, nav_list);
-
 // Listen for clicks on #menu-button and toggle the visible class
 var menu_button = document.querySelector('#menu-button');
 menu_button.addEventListener('click', function(e) {
   nav_list.classList.toggle('visible');
   e.preventDefault();
 });
-
+// click image funtion for gallery
 (document).ready(function(){
   ('.thumb, a').click(function(e){
     e.preventDefault();
